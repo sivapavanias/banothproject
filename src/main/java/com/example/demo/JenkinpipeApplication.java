f867810a764bc4cb9f7452adeefc209e3aaa0ffc
banoth <kbanoth@dxc.com>
2019-04-15 18:02:29 +0530

    update

---
+++ src/main/java/com/example/demo/JenkinpipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinpipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinpipeApplication.class, args);
+	}
+
+}
